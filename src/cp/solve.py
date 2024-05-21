d55c8565a7ab9665b2e921aae936088fe2ab2a99
NotXia <35894453+NotXia@users.noreply.github.com>
2024-05-21 21:53:03 +0200

    Add CP first solution
    Co-authored-by: Rda1027 <Rda1027@users.noreply.github.com>

--- src/cp/solve.py
+++ src/cp/solve.py
@@ -1,17 +1,54 @@
-def solve():
-    return [
-        {
-            "test1": {
-                "time": 300,
-                "optimal": False,
-                "obj": 14,
-                "sol": [ [1] ]
-            },
-            "test2": {
-                "time": 300,
-                "optimal": False,
-                "obj": 16,
-                "sol": [ [1] ]
-            }
-        }
-    ]*21
+from minizinc import Instance, Model, Solver
+from minizinc.result import Status
+import pathlib
+import os
+from datetime import timedelta
+
+
+def solve(instance, timeout, random_seed=42):
+    to_try_models = [
+        Model( os.path.join(pathlib.Path(__file__).parent.resolve(), "./milp_like.mzn") )
+    ]
+    to_try_solvers = [
+        Solver.lookup("gecode"),
+        Solver.lookup("chuffed")
+    ]
+    out_results = []
+
+    for model in to_try_models:
+        for solver in to_try_solvers:
+            instance_data = Instance(solver, model)
+
+            for key, value in instance.items():
+                instance_data[key] = value
+
+            try:
+                result = instance_data.solve(
+                    timeout = timedelta(seconds=timeout),
+                    random_seed = random_seed
+                )
+            except:
+                time = timeout
+                optimal = False
+                objective_value = None
+                sol = None
+            else:
+                time = round(result.statistics["solveTime"].total_seconds()) if ("solveTime" in result.statistics) else timeout
+                optimal = result.status == Status.OPTIMAL_SOLUTION
+                if result.solution is None:
+                    objective_value = None
+                    sol = None
+                else:
+                    objective_value = result["objective"]
+                    sol = [[x for x in sol if x != 0] for sol in result["T"]]
+
+            out_results.append({
+                "milp_like_gecode": {
+                    "time": time,
+                    "optimal": optimal,
+                    "obj": objective_value,
+                    "sol": sol
+                }
+            })
+
+    return out_results
