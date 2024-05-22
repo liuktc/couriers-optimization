9b2d876d4fa7b37b595e7aa4fa0c1e9e76943ca4
NotXia <35894453+NotXia@users.noreply.github.com>
2024-05-22 07:49:56 +0200

    Add logging and results caching

--- src/cp/solve.py
+++ src/cp/solve.py
@@ -3,22 +3,40 @@ from minizinc.result import Status
 import pathlib
 import os
 from datetime import timedelta
+import logging
+logger = logging.getLogger(__name__)
 
 
-def solve(instance, timeout, random_seed=42):
-    to_try_models = [
-        Model( os.path.join(pathlib.Path(__file__).parent.resolve(), "./milp_like.mzn") )
-    ]
-    to_try_solvers = [
-        Solver.lookup("gecode"),
-        Solver.lookup("chuffed")
-    ]
-    out_results = []
+experiments_setup = [
+    {
+        "name": "milp_like",
+        "model": Model( os.path.join(pathlib.Path(__file__).parent.resolve(), "./milp_like.mzn") ),
+        "solvers": ["gecode", "chuffed"]
+    }
+]
 
-    for model in to_try_models:
-        for solver in to_try_solvers:
+
+def solve(instance, timeout, cache={}, random_seed=42):
+    out_results = {}
+
+    for experiment in experiments_setup:
+        logger.info(f"Starting model {experiment['name']}")
+        model = experiment["model"]
+
+        for solver_name in experiment["solvers"]:
+            logger.info(f"Solving with {solver_name}")
+            result_key = f"{experiment['name']}-{solver_name}"
+
+            # Check if result is in cache
+            if result_key in cache:
+                logger.info(f"Cache hit")
+                out_results[result_key] = cache[result_key]
+                continue
+
+            solver = Solver.lookup(solver_name)
             instance_data = Instance(solver, model)
 
+            # Set input data
             for key, value in instance.items():
                 instance_data[key] = value
 
@@ -42,13 +60,11 @@ def solve(instance, timeout, random_seed=42):
                     objective_value = result["objective"]
                     sol = [[x for x in sol if x != 0] for sol in result["T"]]
 
-            out_results.append({
-                "milp_like_gecode": {
-                    "time": time,
-                    "optimal": optimal,
-                    "obj": objective_value,
-                    "sol": sol
-                }
-            })
+            out_results[result_key] = {
+                "time": time,
+                "optimal": optimal,
+                "obj": objective_value,
+                "sol": sol
+            }
 
     return out_results
--- src/solve.py
+++ src/solve.py
@@ -6,15 +6,27 @@ from input_parser import parseInstanceFile
 import argparse
 import os
 import json
+import logging
+logger = logging.getLogger(__name__)
+
+
+def __loadCache(results_file_path):
+    if not os.path.isfile(results_file_path): return {}
+
+    with open(results_file_path, "r") as f:
+        return json.load(f)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Instance solver")
     parser.add_argument("--instances-path", type=str, default="./instances")
-    parser.add_argument("--timeout", type=int, default=300)
+    parser.add_argument("--timeout", type=int, default=300, help="Timeout in seconds")
     parser.add_argument("--output-path", type=str, default="./res")
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("--overwrite-old", action="store_true", help="If set, old results with the same name will be run again")
     args = parser.parse_args()
 
+    logging.basicConfig(level=logging.INFO if args.verbose else logging.WARN)
 
     instances = [ parseInstanceFile(os.path.join(args.instances_path, f)) for f in sorted(os.listdir(args.instances_path)) ]
 
@@ -39,18 +51,31 @@ if __name__ == "__main__":
     ]
 
     for out_dir, solve_fn in output_hierarchy:
-        results = []
+        logger.info(f"Starting processing for {out_dir}")
 
-        for instance in instances:
-            results.append( 
-                solve_fn(
-                    instance = instance,
-                    timeout = args.timeout
-                ) 
-            )
+        for i, instance in enumerate(instances):
+            # Init cache
+            if args.overwrite_old:
+                cached_results = {}
+            else:
+                cached_results = __loadCache(os.path.join(out_dir, f"{i+1}.json"))
 
-        for i, instance_result in enumerate(results):
-            with open(os.path.join(out_dir, f"{i+1}.json"), "w") as f:
-                json.dump(instance_result, f, indent=3)
+            # Solving instance
+            logger.info(f"Starting instance {i+1}")
+            instance_results = solve_fn(
+                instance = instance,
+                timeout = args.timeout,
+                cache = cached_results
+            ) 
+
+            for key in cached_results:
+                if key not in instance_results:
+                    instance_results[key] = cached_results[key]
+
+            # Saving instance results
+            results_file_path = os.path.join(out_dir, f"{i+1}.json")
+            with open(results_file_path, "w") as f:
+                logger.info(f"Saving results in {results_file_path}")
+                json.dump(instance_results, f, indent=3)
 
 
