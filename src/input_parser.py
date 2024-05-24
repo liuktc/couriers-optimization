12b1c122c368a3f00cd2a8567c57c098af818855
NotXia <35894453+NotXia@users.noreply.github.com>
2024-05-24 11:25:42 +0200

    Add input parser for Minizinc

--- src/input_parser.py
+++ src/input_parser.py
@@ -1,4 +1,7 @@
 import re
+import argparse
+import os
+from pathlib import Path
 
 
 def __cleanLine(line):
@@ -22,3 +25,32 @@ def parseInstanceFile(path):
         "s": sizes,
         "D": distances
     }
+
+def __parseForMinizinc(instance):
+    out = ""
+    out += f"m = {instance['m']};\n"
+    out += f"n = {instance['n']};\n"
+    out += f"l = [ {','.join([str(x) for x in instance['l']])} ];\n"
+    out += f"s = [ {','.join([str(x) for x in instance['s']])} ];\n"
+    out += "D = ["
+    for d in instance["D"]:
+        out += f"| {','.join([str(x) for x in d])} "
+    out += "|];\n"
+    
+    return out
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Input parser")
+    parser.add_argument("--instances-path", type=str, default="./instances")
+    parser.add_argument("--output-dir", type=str, required=True)
+    parser.add_argument("--target", type=str, choices=["minizinc"], required=True)
+    args = parser.parse_args()
+    
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    for f_name in os.listdir(args.instances_path):
+        instance = parseInstanceFile(os.path.join(args.instances_path, f_name))
+        with open(os.path.join(args.output_dir, f"{Path(f_name).stem}.dzn"), "w") as f:
+            f.write(__parseForMinizinc(instance))
