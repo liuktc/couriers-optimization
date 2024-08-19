5cd7af75c63b1081627f336924c5db501dbd7af3
Luca <luca24ever@gmail.com>
2024-08-19 18:36:25 +0200

    hopefully fixed import error

--- src/smt/solve.py
+++ src/smt/solve.py
@@ -1,6 +1,6 @@
-from model import SMT
-from model_arrays import SMT_array
-from model_new import SMT_new
+from .model import SMT
+from .model_arrays import SMT_array
+from .model_new import SMT_new
 
 def solve(instance, timeout, **kwargs):
     res_dict = SMT_new(instance["m"], instance["n"], instance["l"], instance["s"], instance["D"], timeout=timeout,**kwargs)
