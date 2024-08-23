d87b60b63ae849c2622e099c6b10d71c936c5f50
NotXia <35894453+NotXia@users.noreply.github.com>
2024-08-23 18:07:08 +0200

    Fix timeout handling

--- src/sat/solve.py
+++ src/sat/solve.py
@@ -1,13 +1,46 @@
 from .total_sat import Unified_Model
 import time
-import signal
+import multiprocessing
 import logging
 logger = logging.getLogger(__name__)
 
 
 
-def signalHandler(signum, frame):
-    raise Exception("Model init timeout")
+def modelRunner(ModelClass, instance, timeout, random_seed, queue):
+    objective = None
+    solution = None
+    optimality = False
+    solve_time = timeout
+
+    try:
+        start_model_init = time.time()
+
+        def_model = ModelClass(instance)
+        model_init_time = round(time.time() - start_model_init)
+        logger.info(f"Model created")
+        
+        objective, solution, optimality, solve_time = def_model.solve(timeout-model_init_time, random_seed)
+        solve_time = solve_time + model_init_time
+    except Exception as e:
+        logger.error(f"Exception {e}")
+
+    result = {}
+
+    if objective is not None:
+        result = {
+            'obj': objective,
+            'sol': solution,
+            'optimal': optimality,
+            'time': solve_time,
+        }
+    else:
+        result = {
+            'obj': None,
+            'sol': None,
+            'optimal': False,
+            'time': timeout,
+        }
+    queue.put(result, block=False)
 
 
 def solve(instance, timeout, cache={}, random_seed=42, **kwargs):
@@ -26,41 +59,26 @@ def solve(instance, timeout, cache={}, random_seed=42, **kwargs):
             logger.info(f"Cache hit")
             results[model] = cache[model]
             continue
-
-        objective = None
-        solution = None
-        optimality = False
-        solve_time = timeout
-
-        # Declaration of the Model class
+        
+        res = None
+        runner_queue = multiprocessing.Queue()
+        proc = multiprocessing.Process(target=modelRunner, args=(models[model], instance, timeout, random_seed, runner_queue))
         try:
-            start_model_init = time.time()
-
-            # Signal to stop model creation on timeout
-            signal.signal(signal.SIGALRM, signalHandler)
-            signal.alarm(timeout)
-            def_model = models[model](instance)
-            signal.alarm(0)
-
-            model_init_time = round(time.time() - start_model_init)
-            logger.info(f"Model created")
-            
-            objective, solution, optimality, solve_time  = def_model.solve(timeout-model_init_time, random_seed)
+            proc.start()
+            res = runner_queue.get(block=True, timeout=timeout+1) # Tolerance to wait for Z3 timeout
         except Exception as e:
             logger.error(f"Exception {e}")
-            
-        if objective is not None:
-            results[model] = {
-                'obj': objective,
-                'sol': solution,
-                'optimal': optimality,
-                'time': solve_time + model_init_time,
-            }
-        else:
+        finally:
+            proc.terminate()
+
+        if res is None:
             results[model] = {
                 'obj': None,
                 'sol': None,
                 'optimal': False,
                 'time': timeout,
             }
+        else:
+            results[model] = res
+
     return results
