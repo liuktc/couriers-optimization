4dea62c0a71e58edddbdb9b4bf75d9d0969936fc
Luca <luca24ever@gmail.com>
2024-08-23 20:54:15 +0200

    redo local search with instance 11

--- src/smt/model_local_search.py
+++ src/smt/model_local_search.py
@@ -1,6 +1,6 @@
 from z3 import *
 import time
-from utils import maximum, precedes, millisecs_left, Min, get_element_at_index, subcircuit, get_best_neighbor, minimum
+from .utils import maximum, precedes, millisecs_left, Min, get_element_at_index, subcircuit, get_best_neighbor, minimum
 import itertools
 import logging
 logger = logging.getLogger(__name__)
